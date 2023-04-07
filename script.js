3e6b85301cb9c2605d3f669fd02bc4fa0a572d04
FernandoAr <fer3700@gmail.com>
2023-04-07 11:31:05 -0300

    Otros cambios mas

--- script.js
+++ script.js
@@ -14,3 +14,24 @@ function currentTime(){
 }
 
 setInterval(currentTime, 1000);
+
+const nextBtn = document.querySelector("#next-btn");
+const backBtn = document.querySelector("#back-btn");
+const step1 = document.querySelector("#step-1");
+const step2 = document.querySelector("#step-2");
+const summary = document.querySelector("#summary");
+
+//agregar evento onclick al botón siguiente
+nextBtn.addEventListener("click", function() {
+  // recopilar datos del primer paso
+  let name = document.querySelector("#name").value;
+  let email = document.querySelector("#email").value;
+  let mensaje = document.querySelector("#msj").value;
+  
+  // mostrar resumen en el segundo paso
+  summary.innerHTML = `Nombre: ${name}<br>Email: ${email}<br> ${mensaje} `;
+  
+  // ocultar el primer paso y mostrar el segundo paso
+  step1.style.display = "none";
+  step2.style.display = "block";
+});
