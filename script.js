34864274dfa1cbe63bfdf7925592efcbbefe0073
FernandoAr <fer3700@gmail.com>
2023-04-10 19:58:20 -0300

    Baje el Nav y agregue una imagen de fondo

--- script.js
+++ script.js
@@ -20,18 +20,25 @@ const backBtn = document.querySelector("#back-btn");
 const step1 = document.querySelector("#step-1");
 const step2 = document.querySelector("#step-2");
 const summary = document.querySelector("#summary");
+const image = document.querySelector("#img"),
+  input = document.querySelector("#upimg");
+
+  input.addEventListener("change", () => {image.src = URL.createObjectURL(input.files[0])});
+
 
 //agregar evento onclick al botón siguiente
 nextBtn.addEventListener("click", function() {
   // recopilar datos del primer paso
+  let upimg = document.querySelector("#upimg").value;
   let name = document.querySelector("#name").value;
   let email = document.querySelector("#email").value;
   let mensaje = document.querySelector("#msj").value;
   
   // mostrar resumen en el segundo paso
-  summary.innerHTML = `Nombre: ${name}<br>Email: ${email}<br> ${mensaje} `;
+  summary.innerHTML = `${upimg} <br> Nombre: ${name}<br>Email: ${email}<br> ${mensaje} `;
   
   // ocultar el primer paso y mostrar el segundo paso
   step1.style.display = "none";
-  step2.style.display = "block";
+  step2.style.display = "block flow";
 });
+
